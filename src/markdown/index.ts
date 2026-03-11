685c2f98f104587b16085da1ab9ede748c269edc
Jeon Suyeol <devxoul@gmail.com>
2026-03-11 18:24:03 +0900

    chore: populate markdown barrel file and fix integration test error message

--- src/markdown/index.ts
+++ src/markdown/index.ts
@@ -1,2 +1,6 @@
-// Markdown ↔ HWP(X) conversion — barrel file
-// Exports will be added as modules are implemented
+export { CharShapeRegistry } from './charshape-registry'
+export { createHeadingInfrastructure, getHeadingLevel } from './heading-styles'
+export { embedImage, extractImages, resolveImagePaths } from './image-handler'
+export { markdownToHwpBinary } from './to-hwp-binary'
+export { markdownToHwp } from './to-hwp'
+export { hwpToMarkdown } from './to-markdown'
